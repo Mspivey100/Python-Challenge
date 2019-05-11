265d84dabcc1711ee9dff5b302a39c2dec499d7a
Mychal Spivey <mychalspivey@Mychals-MacBook-Pro.local>
2019-05-11 19:07:38 -0400

    more stuff works

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,22 +3,37 @@ import os
 
 csvpath = os.path.join('budget_data.csv')
 totalMonth = 0
+totalNet = 0
+average = 0
+maxNum = 0
 
 with open (csvpath,newline='') as csvfile:
    
-   csvreader = csv.reader(csvfile,delimiter=' ')
+   csvreader = csv.reader(csvfile,delimiter=',')
 
    print(csvreader)
 
    csv_header = next(csvreader)
    print(f"Csv header: {csv_header}")
-
+       
    for row in csvreader:
        totalMonth= totalMonth+1
+       totalNet += float(row[1])
+       average = round(totalNet / totalMonth,2)
+       maxNum = max(maxNum,float(row[1]))
+       
+
+
+
+       
+
 
 print(f'Financial Analysis')
 print(f'___________________________')
 print(f'Total Months: {totalMonth}')
+print(f'net total: ${totalNet}')
+print(f'average: ${average}')
+print(f'Greatest Increase in Profits: ${maxNum}')
    
 
 
