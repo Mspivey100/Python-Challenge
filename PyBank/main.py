81e013379cad5f2823a866d6dce7aee7f2a107bf
Mychal Spivey <mychalspivey@Mychals-MacBook-Pro.local>
2019-05-11 17:55:39 -0400

    month total works

--- PyBank/main.py
+++ PyBank/main.py
@@ -2,9 +2,10 @@ import csv
 import os
 
 csvpath = os.path.join('budget_data.csv')
+totalMonth = 0
 
 with open (csvpath,newline='') as csvfile:
-
+   
    csvreader = csv.reader(csvfile,delimiter=' ')
 
    print(csvreader)
@@ -13,4 +14,11 @@ with open (csvpath,newline='') as csvfile:
    print(f"Csv header: {csv_header}")
 
    for row in csvreader:
-       print(row) 
+       totalMonth= totalMonth+1
+
+print(f'Financial Analysis')
+print(f'___________________________')
+print(f'Total Months: {totalMonth}')
+   
+
+
