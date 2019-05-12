2ddda2018438f37c867edb0232fb74c7b4c4b50f
Mychal Spivey <mychalspivey@Mychals-MacBook-Pro.local>
2019-05-12 10:41:29 -0400

    mostly works

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,6 +4,17 @@ import os
 voteCount=0
 khanCount=0
 correyCount=0
+liCount=0
+toolCount=0
+khanPercent = 0
+correyPercent = 0
+liPercent = 0
+toolPercent = 0
+
+def percentCalc(votes):
+    return votes / voteCount
+
+
 
 csvpath = os.path.join('election_data.csv')
 with open (csvpath,newline='') as csvfile:
@@ -18,10 +29,23 @@ with open (csvpath,newline='') as csvfile:
            khanCount = khanCount + 1
        elif row[2] == "Correy":
            correyCount = correyCount + 1
+       elif row[2] == "Li":
+           liCount = liCount + 1
+       elif row[2] == """O'Tooley""":
+           toolCount = toolCount + 1
+        
+       khanPercent = percentCalc(khanCount)
+       correyPercent = percentCalc(correyCount)
+       liPercent = percentCalc(liCount)
+       toolPercent = percentCalc(toolCount)
+       
 
 
 
-print(f'votes: {voteCount}')
-print(f'Khan: {khanCount}')
-print(f'Correy:{correyCount}')
+
+print(f'votes: ({voteCount})')
+print(f'Khan: {khanPercent} ({khanCount})')
+print(f'Correy: {correyPercent} ({correyCount})')
+print(f'Li: {liPercent} ({liCount})')
+print(f"""O'tooley: {toolPercent} ({toolCount})""")
    
